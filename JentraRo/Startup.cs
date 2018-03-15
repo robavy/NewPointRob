5b9ad84f198730f533ee84f2d0531f1c8748cf46
Roberto <roblan.dukan@gmail.com>
2018-03-15 13:37:49 -0600

    1.0.0
    Diseño base de proyectos Rob

---
+++ JentraRo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JentraRo.Startup))]
+namespace JentraRo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
