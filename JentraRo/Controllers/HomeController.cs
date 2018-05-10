0407380d531cca9cf9798ba97b3489a2411f0192
Roberto <roblan.dukan@gmail.com>
2018-05-09 20:43:17 -0500

    File Upload

--- JentraRo/Controllers/HomeController.cs
+++ JentraRo/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,6 +25,31 @@ namespace JentraRo.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        
+        public ActionResult Upload()
+        {
+
+            try
+            {
+                foreach (string upload in Request.Files)
+                {
+                    if (Request.Files[upload].FileName != "")
+                    {
+                        string path = AppDomain.CurrentDomain.BaseDirectory + "/App_Data/uploads/";
+                        string filename = Path.GetFileName(Request.Files[upload].FileName);
+                        Request.Files[upload].SaveAs(Path.Combine(path, filename));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
             return View();
         }
     }
